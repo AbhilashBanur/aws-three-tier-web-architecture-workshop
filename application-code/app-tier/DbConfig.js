dd15ed433a3f57baa5581fc29bb6cecd118bc3db
AbhilashBanur <114465865+AbhilashBanur@users.noreply.github.com>
2024-08-12 17:38:52 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cluster-cpyum0ca8z66.us-east-2.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'threetierarc1234',
+    DB_DATABASE : 'webappdb'
 });
